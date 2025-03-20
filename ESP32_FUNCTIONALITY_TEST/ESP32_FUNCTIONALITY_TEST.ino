c13f2483ffe1d6435723d0d0635a68a8d66285cf
NICHOLAS1076 <nicholasniles0@gmail.com>
2025-03-20 16:19:25 -0400

    Update ESP32_FUNCTIONALITY_TEST.ino

--- ESP32_FUNCTIONALITY_TEST/ESP32_FUNCTIONALITY_TEST.ino
+++ ESP32_FUNCTIONALITY_TEST/ESP32_FUNCTIONALITY_TEST.ino
@@ -1,12 +1,18 @@
 
 void setup() {
   pinMode(0, OUTPUT);
+  pinMode(6, OUTPUT);
   Serial.begin(9600);
-  digitalWrite(0, HIGH);
   
 }
 
 void loop() {
   delay(500);
+  digitalWrite(0, HIGH);
+  digitalWrite(6, LOW);
   Serial.println("Hello World");
+  delay(200);
+  digitalWrite(0, LOW);
+  digitalWrite(6, HIGH);
+  delay(300);
 }
